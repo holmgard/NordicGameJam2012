81cb593907f368c62a158c2624f7d3c86c926299
Christoffer Holmgård <holmgard@gmail.com>
2012-01-28 13:13:47 +0100

    first basic connection to SmartFox socket server

---
+++ UnityIphone/Assets/ConnectionGUI.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System;
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Logging;
+
+public class ConnectionGUI : MonoBehaviour
+{
+	//----------------------------------------------------------
+	// Setup variables
+	//----------------------------------------------------------
+	public string ip = "192.168.1.110";
+	public int port = 9933;
+	private string statusMessage = "";
+	private SmartFox smartFox;
+
+	//----------------------------------------------------------
+	// Called when program starts
+	//----------------------------------------------------------
+	void Start()
+	{
+		smartFox = new SmartFox();
+		smartFox.AddEventListener(SFSEvent.CONNECTION, OnConnection);
+		smartFox.AddLogListener(LogLevel.DEBUG, OnDebugMessage);
+		smartFox.Connect(ip, port);
+	}
+	
+	//----------------------------------------------------------
+	// As Unity is not thread safe, we process the queued up callbacks every physics tick
+	//----------------------------------------------------------
+	void FixedUpdate() {
+		smartFox.ProcessEvents();
+	}
+	
+	//----------------------------------------------------------
+	// Draw GUI every frame
+	//----------------------------------------------------------
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10, 10, 500, 100), "Status: " + statusMessage);
+	}
+
+	//----------------------------------------------------------
+	// Handle connection response from server
+	//----------------------------------------------------------
+	public void OnConnection(BaseEvent evt) {
+		bool success = (bool)evt.Params["success"];
+		if (success) {
+			statusMessage = "Connection succesfull!";
+		} else {
+			statusMessage = "Can't connect!";			
+		}
+	}
+
+	public void OnDebugMessage(BaseEvent evt) {
+		string message = (string)evt.Params["message"];
+		Debug.Log("[SFS DEBUG] " + message);
+	}
+	
+	//----------------------------------------------------------
+	// Disconnect from the socket when shutting down the game
+	//----------------------------------------------------------
+	public void OnApplicationQuit() {
+		smartFox.Disconnect();
+	}
+}
---
+++ UnityIphone/Assets/SmartFoxConnector.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class SmartFoxConnector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI()
+	{
+		GUI.Box(new Rect(10,10,100,100),"Hello game jam");	
+	}
+}
